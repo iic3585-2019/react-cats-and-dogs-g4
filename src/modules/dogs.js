dbe92d113b07473a9cbaa1e5a94a6f196c70705e
Luis Chodiman <lechodiman@uc.cl>
2019-05-25 19:25:54 -0700

    Add dogs state

---
+++ src/modules/dogs.js
@@ -0,0 +1,37 @@
+export const GET_DOGS = 'dogs/GET_DOGS';
+
+const initialState = {
+  count: 0,
+  loading: true,
+  dogs: []
+};
+
+export default (state = initialState, action) => {
+  const { payload, type } = action;
+  switch (type) {
+    case GET_DOGS:
+      return {
+        ...state,
+        loading: false,
+        dogs: [...state.dogs, ...payload]
+      };
+
+    default:
+      return {
+        ...state
+      };
+  }
+};
+
+export const fetchDogs = () => async dispatch => {
+  const response = await fetch(
+    'https://dog.ceo/api/breed/hound/images/random/3'
+  ).then(res => res.json());
+
+  const dogs = response.message;
+
+  return dispatch({
+    type: GET_DOGS,
+    payload: dogs
+  });
+};
