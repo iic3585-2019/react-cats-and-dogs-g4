2df03a8609aad4d2b2d613504b9f54f364376433
Luis Chodiman <lechodiman@uc.cl>
2019-05-28 19:28:38 -0700

    Add Favorite tests

--- src/components/layout/Favorite.js
+++ src/components/layout/Favorite.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Image from '../images/Image';
+import PropTypes from 'prop-types';
 
 const mapStateToProps = state => ({
   favorites: state.favorites.favorites
 });
 
-class Favorite extends Component {
+export class Favorite extends Component {
   render() {
     const favs = this.props.favorites.map(f => (
       <Image key={'f-' + f.id} url={f.url} animal={f.animal} id={f.id} fav />
@@ -14,6 +15,10 @@ class Favorite extends Component {
     return <div className="images">{favs}</div>;
   }
 }
+
+Favorite.propTypes = {
+  favorites: PropTypes.array.isRequired
+};
 
 export default connect(
   mapStateToProps,
